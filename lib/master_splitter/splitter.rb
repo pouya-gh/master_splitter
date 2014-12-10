3501a59a1bdba291f650610b852321d4b8a023d7
Pouya Gharib Pour <p.gharibpour@gmail.com>
2014-12-10 15:27:22 +0330

    add standard_splitter method

--- lib/master_splitter/splitter.rb
+++ lib/master_splitter/splitter.rb
@@ -1,4 +1,23 @@
 module MasterSplitter
+  def standard_splitter(source_file_name, number_of_slices, options={})
+    slice_sizes = []
+    slice_names = []
+    output_dir = options[:output_dir] || ""
+    source = File.open(source_file_name, 'rb')
+    source_size = source.size
+    slice_size = source_size / number_of_slices
+
+    number_of_slices.times do |n|
+      slice_sizes << slice_size
+      temp = ("%3d"%[n + 1]).gsub(" ", "0")
+      slice_names << File.join(output_dir, [source_file_name, temp].join('.'))
+    end
+    remain_bytes = source_size - (slice_size * number_of_slices)
+    slices_sizes[-1] +=  remain_bytes
+
+    source.close
+    split(source_file_name, slice_names, slice_sizes)
+  end
 
   def split(source_file_name, slice_names, slices_sizes)
     source = File.open(source_file_name, 'rb')
