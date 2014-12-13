60b27303d8156660be8ca97dc685f9d18db7c339
Pouya Gharib Pour <p.gharibpour@gmail.com>
2014-12-13 23:56:18 +0330

    fix STANDARD_SLICE_NAMING_FORMAT

--- lib/master_splitter/constants.rb
+++ lib/master_splitter/constants.rb
@@ -1,5 +1,5 @@
 module MasterSplitter
   MAX_CHUNK_SIZE = 1024 * 1024
-  STANDARD_SLICE_NAMING_FORMAT = /^(.*)\.(\d{3, })$/i
+  STANDARD_SLICE_NAMING_FORMAT = /^(.*)\.(\d{3,})$/i
   FILE_NAME_FINDER = /^.*\/(.*)$/i
 end
